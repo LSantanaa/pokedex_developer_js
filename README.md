74a27097a780690601e32ffa5a7578f47aaeb4dd
Leonardo <leonardodesantana05@gmail.com>
2023-04-02 12:43:44 -0300

    projeto em desenvolvimento

---
+++ README.md
@@ -0,0 +1,9 @@
+# Trilha JS Developer - Pokedex
+## Bootcamp Potência Tech by IFOOD (ANGULAR)
+
+## SOBRE
+
+O Projeto base da pokedex foi desenvolvido durante o a trilha JS Developer na Digital Innovation One, e utilazado também no bootcamp Potência Tech by Ifood, onde foi proposto o desafio de fazer melhorias e modificações conforme fosse necessário, além de criar os cards com informações mais detalhadas dos pokemons, fazendo as requests necessárias da API PokeAPI disponível [aqui](https://pokeapi.co/).
+
+Esta é a minha versão do projeto onde pude entender melhor sobre as requisições http. Tentei manter um pouco do layout original de inspiração do projeto mas fiz algumas modificações para melhorar a usabilidade.
+
---
+++ assets/js/poke-api.js
@@ -0,0 +1,35 @@
+
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokeDetail['sprites']['versions']['generation-v']['black-white']['animated'].front_default ? pokemon.photo = pokeDetail['sprites']['versions']['generation-v']['black-white']['animated'].front_default : pokemon.photo = pokeDetail.sprites.front_default;
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = async (pokemon) => {
+    const response = await fetch(pokemon.url)
+    const pokeDetail = await response.json()
+    return convertPokeApiDetailToPokemon(pokeDetail)
+}
+
+pokeApi.getPokemons = async (offset = 640, limit = 12) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    const response = await fetch(url)
+    const jsonBody = await response.json()
+    const pokemons = jsonBody.results
+    const detailRequests = pokemons.map(pokeApi.getPokemonDetail)
+    const pokemonsDetails = await Promise.all(detailRequests)
+    return pokemonsDetails
+}
